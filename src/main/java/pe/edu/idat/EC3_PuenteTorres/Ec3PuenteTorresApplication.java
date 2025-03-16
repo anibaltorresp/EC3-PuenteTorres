ea311f1e0ca5225eccf66bba9a7b086cc4f8f9d6
anibaltorresp <anibal.torresp@gmail.com>
2025-03-15 19:31:32 -0500

    initial project EC3

---
+++ src/main/java/pe/edu/idat/EC3_PuenteTorres/Ec3PuenteTorresApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.EC3_PuenteTorres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec3PuenteTorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec3PuenteTorresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/EC3_PuenteTorres/Ec3PuenteTorresApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.EC3_PuenteTorres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec3PuenteTorresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
