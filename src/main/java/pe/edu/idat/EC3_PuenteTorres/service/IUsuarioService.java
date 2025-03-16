26a563210c38c3a83debc5051679d022f2735bf8
AugustoAlonzo <augustoalonzo22@gmail.com>
2025-03-15 23:18:05 -0500

    creando IUsuarioService

---
+++ src/main/java/pe/edu/idat/EC3_PuenteTorres/service/IUsuarioService.java
@@ -0,0 +1,8 @@
+package pe.edu.idat.EC3_PuenteTorres.service;
+
+
+import pe.edu.idat.EC3_PuenteTorres.model.Usuario;
+
+public interface IUsuarioService {
+    Usuario obtenerUsuarioxNomusuario(String nomusuario);
+}
