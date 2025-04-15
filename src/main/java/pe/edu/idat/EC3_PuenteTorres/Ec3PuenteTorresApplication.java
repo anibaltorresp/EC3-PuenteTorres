49a5c5a92b22d3836d21c6f78dddc9ca3b8b38ab
anibaltorresp <anibal.torresp@gmail.com>
2025-04-14 19:19:19 -0500

    Implementando Cors

--- src/main/java/pe/edu/idat/EC3_PuenteTorres/Ec3PuenteTorresApplication.java
+++ src/main/java/pe/edu/idat/EC3_PuenteTorres/Ec3PuenteTorresApplication.java
@@ -2,6 +2,9 @@ package pe.edu.idat.EC3_PuenteTorres;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Ec3PuenteTorresApplication {
@@ -10,4 +13,15 @@ public class Ec3PuenteTorresApplication {
 		SpringApplication.run(Ec3PuenteTorresApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "PATCH");
+			}
+		};
+	}
 }
